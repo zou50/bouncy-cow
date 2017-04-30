d50576ca531d93c3062c31664aa07d66a0512c93
Danny Zou <zou50@purdue.edu>
2017-04-30 15:23:59 -0400

    Added tube sprites and object

---
+++ core/src/com/zou50/bouncycow/sprites/Tube.java
@@ -0,0 +1,45 @@
+package com.zou50.bouncycow.sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.Random;
+
+/**
+ * Created by Danny on 4/30/17.
+ */
+public class Tube {
+
+    private static final int FLUCTUATION = 130;
+    private static final int TUBE_GAP = 100;
+    private static final int LOWEST_OPENING = 120;
+    private Texture topTube, bottomTube;
+    private Vector2 posTopTube, posBottomTube;
+    private Random rand;
+
+    public Tube(float x) {
+        topTube = new Texture("android/assets/toptube.png");
+        bottomTube = new Texture("android/assets/bottomtube.png");
+        rand = new Random();
+
+        posTopTube = new Vector2(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENING);
+        posBottomTube = new Vector2(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+    }
+
+    public Texture getTopTube() {
+        return topTube;
+    }
+
+    public Texture getBottomTube() {
+        return bottomTube;
+    }
+
+    public Vector2 getPosTopTube() {
+        return posTopTube;
+    }
+
+    public Vector2 getPosBottomTube() {
+        return posBottomTube;
+    }
+
+}
--- core/src/com/zou50/bouncycow/states/PlayState.java
+++ core/src/com/zou50/bouncycow/states/PlayState.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.zou50.bouncycow.BCGame;
 import com.zou50.bouncycow.sprites.Cow;
+import com.zou50.bouncycow.sprites.Tube;
 
 /**
  * Created by Danny on 4/30/17.
@@ -13,12 +14,14 @@ public class PlayState extends State {
 
     private Cow cow;
     private Texture bg;
+    private Tube tube;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         cow = new Cow(50, 300);
         cam.setToOrtho(false, BCGame.WIDTH / 2, BCGame.HEIGHT / 2);
         bg = new Texture("android/assets/bg.png");
+        tube = new Tube(100);
     }
 
     @Override
@@ -39,6 +42,8 @@ public class PlayState extends State {
         sb.begin();
         sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), 0);
         sb.draw(cow.getTexture(), cow.getPosition().x, cow.getPosition().y);
+        sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+        sb.draw(tube.getBottomTube(), tube.getPosBottomTube().x, tube.getPosBottomTube().y);
         sb.end();
     }
 
