12e313c337609c6be2fa5591516a4bd762759ab5
Danny Zou <dannyzou@Dannys-MacBook-Pro.local>
2017-04-29 23:51:50 -0400

    Initial commit

---
+++ android/src/com/zou50/bouncycow/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.zou50.bouncycow;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.zou50.bouncycow.BCGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BCGame(), config);
+	}
+}
---
+++ desktop/src/com/zou50/bouncycow/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.zou50.bouncycow.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.zou50.bouncycow.BCGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BCGame(), config);
+	}
+}
