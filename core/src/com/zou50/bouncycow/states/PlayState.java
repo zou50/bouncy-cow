ce2dc01d4a39941f2b3d6c89cf9650fc2ad1d2d7
Danny Zou <zou50@purdue.edu>
2017-04-30 01:13:56 -0400

    Added play state

--- core/src/com/zou50/bouncycow/states/PlayState.java
+++ core/src/com/zou50/bouncycow/states/PlayState.java
@@ -1,7 +1,40 @@
 package com.zou50.bouncycow.states;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 /**
  * Created by Danny on 4/30/17.
  */
-public class PlayState {
+public class PlayState extends State {
+
+    private Texture cow;
+
+    public PlayState(GameStateManager gsm) {
+        super(gsm);
+        cow = new Texture("android/assets/cow.png");
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(cow, 50, 50);
+        sb.end();
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
 }
