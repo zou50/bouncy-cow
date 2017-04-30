7216834a4d0dcb73829f14f476ba70fb57d21fd8
Danny Zou <zou50@purdue.edu>
2017-04-30 16:03:43 -0400

    Added ground and collision

--- core/src/com/zou50/bouncycow/states/PlayState.java
+++ core/src/com/zou50/bouncycow/states/PlayState.java
@@ -3,6 +3,7 @@ package com.zou50.bouncycow.states;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.zou50.bouncycow.BCGame;
 import com.zou50.bouncycow.sprites.Cow;
@@ -15,9 +16,12 @@ public class PlayState extends State {
 
     private static final int TUBE_SPACING = 125;
     private static final int TUBE_COUNT = 4;
+    private static final int GROUND_Y_OFFSET = -50;
 
     private Cow cow;
     private Texture bg;
+    private Texture ground;
+    private Vector2 groundPos1, groundPos2;
 
     private Array<Tube> tubes;
 
@@ -26,6 +30,9 @@ public class PlayState extends State {
         cow = new Cow(50, 300);
         cam.setToOrtho(false, BCGame.WIDTH / 2, BCGame.HEIGHT / 2);
         bg = new Texture("android/assets/bg.png");
+        ground = new Texture("android/assets/ground.png");
+        groundPos1 = new Vector2(cam.position.x - cam.viewportWidth / 2, GROUND_Y_OFFSET);
+        groundPos2 = new Vector2((cam.position.x - cam.viewportWidth / 2) + ground.getWidth(), GROUND_Y_OFFSET);
 
         tubes = new Array<Tube>();
         for (int i = 1; i <= TUBE_COUNT; i++)
@@ -42,6 +49,7 @@ public class PlayState extends State {
     @Override
     public void update(float dt) {
         handleInput();
+        updateGround();
         cow.update(dt);
         cam.position.x = cow.getPosition().x + 80;
 
@@ -55,6 +63,8 @@ public class PlayState extends State {
             }
         }
 
+        if (cow.getPosition().y <= ground.getHeight() + GROUND_Y_OFFSET)
+            gsm.set(new PlayState(gsm));
         cam.update();
     }
 
@@ -68,6 +78,8 @@ public class PlayState extends State {
             sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
             sb.draw(tube.getBotTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
         }
+        sb.draw(ground, groundPos1.x, groundPos1.y);
+        sb.draw(ground, groundPos2.x, groundPos2.y);
         sb.end();
     }
 
@@ -77,6 +89,13 @@ public class PlayState extends State {
         cow.dispose();
         for (Tube tube: tubes)
             tube.dispose();
+    }
+
+    private void updateGround() {
+        if (cam.position.x - (cam.viewportWidth / 2) > groundPos1.x + ground.getWidth())
+            groundPos1.add(ground.getWidth() * 2, 0);
+        if (cam.position.x - (cam.viewportWidth / 2) > groundPos2.x + ground.getWidth())
+            groundPos2.add(ground.getWidth() * 2, 0);
     }
 
 }
