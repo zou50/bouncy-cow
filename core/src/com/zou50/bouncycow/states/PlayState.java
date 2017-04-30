aa8f5de0fa963c18fdc3adf82c5ab73df88683e1
Danny Zou <zou50@purdue.edu>
2017-04-30 01:51:44 -0400

    Updated play state cam

--- core/src/com/zou50/bouncycow/states/PlayState.java
+++ core/src/com/zou50/bouncycow/states/PlayState.java
@@ -2,6 +2,7 @@ package com.zou50.bouncycow.states;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.zou50.bouncycow.BCGame;
 
 /**
  * Created by Danny on 4/30/17.
@@ -13,6 +14,7 @@ public class PlayState extends State {
     public PlayState(GameStateManager gsm) {
         super(gsm);
         cow = new Texture("android/assets/cow.png");
+        cam.setToOrtho(false, BCGame.WIDTH / 2, BCGame.HEIGHT / 2);
     }
 
     @Override
@@ -27,6 +29,7 @@ public class PlayState extends State {
 
     @Override
     public void render(SpriteBatch sb) {
+        sb.setProjectionMatrix(cam.combined);
         sb.begin();
         sb.draw(cow, 50, 50);
         sb.end();
